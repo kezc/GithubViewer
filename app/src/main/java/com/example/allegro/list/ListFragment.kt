b361d015dd5d33c80d15377b98d585546e3fafff
Wojtek <krkezc@gmail.com>
2021-04-21 18:20:24 +0200

    open repo in browser, sort list menu

--- app/src/main/java/com/example/allegro/list/ListFragment.kt
+++ app/src/main/java/com/example/allegro/list/ListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.allegro.list
 
+import android.app.Dialog
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -13,6 +15,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.allegro.R
+import com.example.allegro.api.GithubService
 import com.example.allegro.databinding.FragmentListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,18 +23,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class ListFragment : Fragment(R.layout.fragment_list) {
     private val viewModel: ListViewModel by viewModels()
 
-    private var _binding: FragmentListBinding? = null
-    private val binding get() = _binding!!
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        _binding = FragmentListBinding.bind(view)
+        val binding = FragmentListBinding.bind(view)
 
-        val adapter = GithubRepositoriesAdapter { repository, binding ->
+        val adapter = GithubRepositoriesAdapter { repository, itemBinding ->
             val extras = FragmentNavigatorExtras(
-                binding.textViewName to "name_${repository.name}",
+                itemBinding.textViewName to "name_${repository.name}",
             )
             findNavController().navigate(
                 ListFragmentDirections.toDetailsFragment(repository), extras
@@ -74,10 +73,46 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         }
 
         viewModel.repositories.observe(viewLifecycleOwner) {
+            binding.recyclerView.scrollToPosition(0)
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
 
         setHasOptionsMenu(true)
+    }
+
+    private fun createDialog(): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+
+            val currentlySelectedIndex = when (viewModel.currentQuery.value) {
+                GithubService.SortOptions.FULL_NAME -> 0
+                GithubService.SortOptions.CREATED -> 1
+                GithubService.SortOptions.UPDATED -> 2
+                GithubService.SortOptions.PUSHED -> 3
+                else -> 0
+            }
+
+            builder.setTitle(R.string.choose_order)
+                .setSingleChoiceItems(R.array.sort_orders, currentlySelectedIndex, null)
+                .setPositiveButton(R.string.ok) { dialog, _ ->
+                    val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
+
+                    val selectedOption = when (selectedPosition) {
+                        0 -> GithubService.SortOptions.FULL_NAME
+                        1 -> GithubService.SortOptions.CREATED
+                        2 -> GithubService.SortOptions.UPDATED
+                        3 -> GithubService.SortOptions.PUSHED
+                        else -> GithubService.SortOptions.FULL_NAME
+                    }
+                    viewModel.changeSortOrder(selectedOption)
+                    dialog.dismiss()
+                }
+                .setNegativeButton(R.string.cancel) { dialog, _ ->
+                    dialog.dismiss()
+                }
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -85,8 +120,12 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         inflater.inflate(R.menu.sort_menu, menu)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.sorting_order) {
+            createDialog().show()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
+
 }
